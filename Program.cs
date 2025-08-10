ade126a987e09613033cb96693f7d9814086e5e8
Supriya Bandela <supriyabandela1@gmail.com>
2025-08-10 13:01:32 -0400

    updated program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowReactApp",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000")
+            policy.WithOrigins("http://localhost:3000", https://dessertsapp.onrender.com/)
                           .AllowAnyMethod()
                           .AllowAnyHeader();
                           });
@@ -20,6 +20,12 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Get the port from environment variable (Render sets this automatically)
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+
+// Tell Kestrel to listen on this port on all network interfaces (0.0.0.0)
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 var app = builder.Build();
 
